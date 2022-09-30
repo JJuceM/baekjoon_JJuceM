25078f0fc0727adf123eab55e52e57bf3ba66b68
JJuceM <dbqlsl1209@naver.com>
2022-10-01 01:36:52 +0900

    해결(5,6,7,8)

---
+++ src/supernoob/N2557.java
@@ -0,0 +1,13 @@
+package supernoob;
+
+import java.io.*;
+
+public class N2557 {
+    public static void main(String[] args) throws IOException{
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String str = "Hello World!";
+        bw.write(str);
+        bw.close();
+    }
+}
+
